27b0855ef9ec981b82af62b4ed51dfc76f822e8a
Woot <129883595+WootInABoot@users.noreply.github.com>
2024-02-01 12:00:59 -0500

    Update RockController.cs
    updated rock spawn logic

--- Assets/Scripts/RockController.cs
+++ Assets/Scripts/RockController.cs
@@ -8,36 +8,51 @@ public class RockController : MonoBehaviour
 {
     public float forceMultiplier;
     GameObject player;
-    public float collectionDistance = 0.5f;
+    public float collectionDistance = 0.9f;
     Rigidbody2D rb;
     public AudioSource audioSource;
     public AudioClip pickup;
+    bool isSpawning;
 
 
     private void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player");
         rb = GetComponent<Rigidbody2D>();
+        isSpawning = true;
+        StartCoroutine(SpawnRock_IE());
     }
 
     private void Update()
     {
-        float distance = Math.Abs(Vector2.Distance(player.transform.position, transform.position));
-        Vector3 normalDirection = Vector3.Normalize(player.transform.position - transform.position);
-        if (distance < collectionDistance)
+        if (!isSpawning)
         {
-            rb.AddForce(normalDirection * forceMultiplier, ForceMode2D.Force);
-        }
+            float distance = Math.Abs(Vector2.Distance(player.transform.position, transform.position));
+            Vector3 normalDirection = Vector3.Normalize(player.transform.position - transform.position);
+            if (distance < collectionDistance)
+            {
+                rb.AddForce(normalDirection * forceMultiplier, ForceMode2D.Force);
+            }
+        } 
     }
 
 
     void OnCollisionEnter2D(Collision2D collision)
     {
-        Debug.Log("Collision detected");
-
-        if(collision.gameObject.tag == "Player")
+        if (!isSpawning)
         {
-            Destroy(gameObject);
+            Debug.Log("Collision detected");
+
+            if (collision.gameObject.tag == "Player")
+            {
+                Destroy(gameObject);
+            }
         }
+    }
+
+    IEnumerator SpawnRock_IE()
+    {
+        yield return new WaitForSeconds(0.4f);
+        isSpawning = false;
     }
 }
